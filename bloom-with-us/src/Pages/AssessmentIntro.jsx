54819dc69b57377829f2e1dc766daa3b21bd94d9
Sammy Tran <sammy.tran1@ucalgary.ca>
2024-05-20 19:14:32 -0600

    Added button functionality

--- bloom-with-us/src/Pages/AssessmentIntro.jsx
+++ bloom-with-us/src/Pages/AssessmentIntro.jsx
@@ -1,6 +1,11 @@
 import { Button } from "../components/button";
+import { Link } from "react-router-dom";
 
 export default function AssessmentIntro() {
+  function handleNextPressed() {
+    console.log("Button pressed");
+  }
+
   return (
     <div className="flex flex-col p-20">
       <h1 className="text-3xl text-start pb-8">Hello, Bloom with Us!</h1>
@@ -9,10 +14,16 @@ export default function AssessmentIntro() {
         skills assessment to see what skills you already possess and what skills
         we can help you improve upon.
       </p>
-      <Button variant="gray" className="w-20 self-center">
-        {" "}
-        Next{" "}
-      </Button>
+      <Link to={"/customer-success-manager"}>
+        <Button
+          variant="gray"
+          className="w-20 self-center"
+          onPress={handleNextPressed}
+        >
+          {" "}
+          Next{" "}
+        </Button>
+      </Link>
     </div>
   );
 }
