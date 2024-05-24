e1fbf9e1252229619d72dac1a69f0db78d04be27
Sammy Tran <sammy.tran1@ucalgary.ca>
2024-05-23 22:22:16 -0600

    Updated styling specfied by design

--- bloom-with-us/src/Pages/AssessmentIntro.jsx
+++ bloom-with-us/src/Pages/AssessmentIntro.jsx
@@ -11,16 +11,16 @@ export default function AssessmentIntro() {
 
   return (
     <div className="flex flex-col p-20">
-      <h1 className="text-3xl text-start pb-8">
+      <h1 className="text-5xl text-start pb-8">
         Hello, We&apos;re Bloom with Us!
       </h1>
-      <p className="text-start pb-8">
+      <p className="text-base text-start pb-8">
         We’re here to help you advance your career goals. Start by taking our
         skills assessment to see what skills you already possess and what skills
         we can help you improve upon.
       </p>
       <Button
-        variant="gray"
+        variant="primaryYellow"
         className="w-20 self-center"
         onPress={handleNextPressed}
       >
