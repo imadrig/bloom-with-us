bf42595daf7d0ad3f5904d21f2ff4f970c9be5be
Sammy Tran <sammy.tran1@ucalgary.ca>
2024-05-23 22:19:46 -0600

    Updated button to use useNavigate

--- bloom-with-us/src/Pages/AssessmentIntro.jsx
+++ bloom-with-us/src/Pages/AssessmentIntro.jsx
@@ -1,29 +1,32 @@
 import { Button } from "../components/button";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function AssessmentIntro() {
+  const navigate = useNavigate();
+
   function handleNextPressed() {
     console.log("Button pressed");
+    navigate("/skill-assessment");
   }
 
   return (
     <div className="flex flex-col p-20">
-      <h1 className="text-3xl text-start pb-8">Hello, Bloom with Us!</h1>
+      <h1 className="text-3xl text-start pb-8">
+        Hello, We&apos;re Bloom with Us!
+      </h1>
       <p className="text-start pb-8">
         We’re here to help you advance your career goals. Start by taking our
         skills assessment to see what skills you already possess and what skills
         we can help you improve upon.
       </p>
-      <Link to={"/skill-assessment"}>
-        <Button
-          variant="gray"
-          className="w-20 self-center"
-          onPress={handleNextPressed}
-        >
-          {" "}
-          Next{" "}
-        </Button>
-      </Link>
+      <Button
+        variant="gray"
+        className="w-20 self-center"
+        onPress={handleNextPressed}
+      >
+        {" "}
+        Next{" "}
+      </Button>
     </div>
   );
 }
